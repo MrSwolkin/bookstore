c39642b2da6c37217397d776da5b6bb20efe97e3
ErickSwolkin <swolkinerick@gmail.com>
2024-04-12 21:48:59 +0200

    feat: wip

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -153,7 +153,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", default=0)
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
